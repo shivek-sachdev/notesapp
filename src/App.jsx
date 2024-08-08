842e1d550ea1766956edf996149f76b6fa3275db
shivek.sachdev <shivek@tis.co.th>
2024-08-08 22:49:40 +0700

    Revert to last working version

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,16 @@
-import React from 'react';
-import { Amplify } from 'aws-amplify';
-import { Authenticator, Button, Heading, TextField, View } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-import awsExports from './aws-exports';
-import './AuthStyles.css';
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
-Amplify.configure(awsExports);
-
-const formFields = {
-  signUp: {
-    email: {
-      order: 1,
-    },
-    password: {
-      order: 2,
-    },
-    confirm_password: {
-      order: 3,
-    },
-  },
-};
-
-const components = {
-  SignUp: {
-    Header() {
-      return (
-        <Heading level={3} padding={`1rem`}>
-          Create an Account
-        </Heading>
-      );
-    },
-    Footer() {
-      return (
-        <View textAlign="center" padding={`1rem`}>
-          <Button type="submit" variation="primary">
-            Sign Up
-          </Button>
-        </View>
-      );
-    },
-  },
-};
-
-export default function App() {
+function App() {
   return (
-    <View className="App">
-      <Authenticator
-        formFields={formFields}
-        components={components}
-        signUpAttributes={['email']}
-      >
-        {({ signOut }) => (
-          <View padding="1rem" backgroundColor="#f0f0f0" borderRadius="8px">
-            <Heading level={4}>Welcome! You're signed in.</Heading>
-            <Button onClick={signOut} variation="link">
-              Sign Out
-            </Button>
-          </View>
-        )}
-      </Authenticator>
-    </View>
+    <div className="App">
+      <header className="App-header">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+
+        <h1>Hello from Amplify</h1>
+      </header>
+    </div>
   );
 }
+
+export default App;
